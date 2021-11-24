5cda8b99dbc0f8cf7d6dbf2ac8b4e867498fbccc
williams-jacobr <williams.jacobr@gmail.com>
2021-11-24 17:34:39 +0200

    splitting up into further components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,7 @@
 import "./ExpenseItem.css";
 
+import ExpenseDate from "./ExpenseDate";
+
 function ExpenseItem(props) {
   //   const expenseDate = new Date(2021, 2, 28);
   //   const expenseTitle = "Car Insurance";
@@ -7,7 +9,7 @@ function ExpenseItem(props) {
 
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
