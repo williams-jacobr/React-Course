f4e9c533cced84a61d6828811435fda1d386f1b0
williams-jacobr <williams.jacobr@gmail.com>
2022-01-17 12:46:13 +0200

    working with contexts and context hook

--- User Login/src/components/Login/Login.js
+++ User Login/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState, useReducer } from "react";
+import React, { useEffect, useState, useReducer, useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../../store/auth-context";
 
 const emailReducer = (state, action) => {
   if (action.type === "USER_INPUT") {
@@ -40,6 +41,8 @@ const Login = (props) => {
     value: "",
     isValid: null,
   });
+
+  const authCtx = useContext(AuthContext);
 
   // useEffect(() => {
   //   console.log("EFFECT RUNNING");
@@ -80,7 +83,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
