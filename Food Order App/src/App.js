91e72a1ce08d25ed7e1021670d71cc3b392b0aa6
williams-jacobr <williams.jacobr@gmail.com>
2022-01-19 14:51:53 +0200

    finishing off UI

--- Food Order App/src/App.js
+++ Food Order App/src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meal";
 
 function App() {
+  const [cartShown, setCartShown] = useState(false);
+
+  const showCartHander = function () {
+    setCartShown(true);
+  };
+
+  const hideCartHandler = function () {
+    setCartShown(false);
+  };
+
   return (
     <React.Fragment>
-      <Header />
+      {cartShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHander} />
       <main>
         <Meals />
       </main>
--- Food Order App/src/components/Layout/HeaderCartButton.js
+++ Food Order App/src/components/Layout/HeaderCartButton.js
@@ -3,7 +3,7 @@ import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = function (props) {
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
