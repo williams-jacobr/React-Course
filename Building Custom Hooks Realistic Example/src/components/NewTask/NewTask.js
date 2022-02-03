54840dee7207fcabd0d91d62c112fe4be8b64520
williams-jacobr <williams.jacobr@gmail.com>
2022-02-03 13:03:43 +0200

    building an http request custom hook

---
+++ Building Custom Hooks Realistic Example/src/components/NewTask/NewTask.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import useHttp from "../../hooks/use-http";
+
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
+
+const NewTask = (props) => {
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
+
+  const createTask = function (taskText, taskData) {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
+
+  const enterTaskHandler = async (taskText) => {
+    sendTaskRequest(
+      {
+        url: "https://react-http-bf455-default-rtdb.europe-west1.firebasedatabase.app/tasks.json",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: { text: taskText },
+      },
+      createTask.bind(null, taskText)
+    );
+  };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
