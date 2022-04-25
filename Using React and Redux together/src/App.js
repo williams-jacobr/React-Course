3088eb2999b67bea316bd505cd08a0e98d102b30
williams-jacobr <williams.jacobr@gmail.com>
2022-04-25 20:15:10 +0100

    Working with Redux toolkit and creating multiple slices

--- Using React and Redux together/src/App.js
+++ Using React and Redux together/src/App.js
@@ -1,9 +1,24 @@
-import Counter from './components/Counter';
-
+import { Fragment, useEffect } from "react";
+import Counter from "./components/Counter";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
+import { useSelector } from "react-redux";
 
 function App() {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
+  // useEffect(() => {
+  //   console.log(auth);
+  // }, [auth]);
+
   return (
-    <Counter />
+    <Fragment>
+      <Header />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
+      <Counter />
+    </Fragment>
   );
 }
 
--- Using React and Redux together/src/store/index.js
+++ Using React and Redux together/src/store/index.js
@@ -1,33 +1,12 @@
-import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./auth-slice";
+import counterReducer from "./counter-slice";
 
-const initialState = { counter: 0, showCounter: true };
-
-const counterReducer = (state = initialState, action) => {
-  if (action.type === "increment") {
-    return { counter: state.counter + 1, showCounter: state.showCounter };
-  }
-
-  if (action.type === "increase") {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === "decrement") {
-    return { counter: state.counter - 1, showCounter: state.showCounter };
-  }
-
-  if (action.type === "toggle") {
-    return {
-      showCounter: !state.showCounter,
-      counter: state.counter,
-    };
-  }
-
-  return state;
-};
-
-const store = createStore(counterReducer);
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+    auth: authReducer,
+  },
+});
 
 export default store;
